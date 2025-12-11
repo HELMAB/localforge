26762e65433d86b1e585ee407d97d2832f02c6e0
Mab Hel <h-mab@beniten.com>
2025-12-11 14:30:54 +0700

    feat: remove AppImage target from Linux build configuration and display version in footer

--- src/renderer/src/components/layout/AppFooter.vue
+++ src/renderer/src/components/layout/AppFooter.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="text-center text-gray-600 dark:text-gray-400 text-sm transition-colors">
     <p>{{ t('footer') }}</p>
+    <p class="text-xs mt-1">v{{ version }}</p>
   </div>
 </template>
 
 <script setup>
 import { useI18n } from 'vue-i18n'
+import packageJson from '../../../../../package.json'
 
 const { t } = useI18n()
+const version = packageJson.version
 </script>
