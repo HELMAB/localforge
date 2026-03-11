4ac9c0b518dab5b066856d128758f8766e6e5dd8
Mab Hel <h-mab@beniten.com>
2026-03-11 16:21:04 +0700

    feat: US-003 - Move utils unit tests

--- vitest.config.js
+++ vitest.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    include: ['src/**/*.test.js', 'src/**/*.spec.js'],
+    include: ['src/**/*.test.js', 'src/**/*.spec.js', 'tests/**/*.test.js', 'tests/**/*.spec.js'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
